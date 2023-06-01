afc06984a76d3a67f73c44a7b3a31808b115ad0e
GiladLavi <giladlavi@gmail.com>
2023-06-01 13:53:14 +0300

    Refactor

--- Presentor/Presentor/MainWindow.xaml.cs
+++ Presentor/Presentor/MainWindow.xaml.cs
@@ -27,63 +27,59 @@ namespace Presentor
     /// </summary>
     public partial class MainWindow : Window
     {
-        private IConnection conn;
-        private IModel channel;
         public MainWindow()
         {
             var factory = new ConnectionFactory { HostName = "localhost" };
-            this.conn = factory.CreateConnection();
-            this.channel = this.conn.CreateModel();
-            var result = this.channel.QueueDeclare(queue: "", exclusive: true);
-            this.channel.QueueBind(exchange: "coordinates", queue: result.QueueName, routingKey: "");
+            var conn = factory.CreateConnection();
+            var channel = conn.CreateModel();
+            // we create a temporary queue
+            var result = channel.QueueDeclare(queue: "", exclusive: true);
+            channel.QueueBind(exchange: "coordinates", queue: result.QueueName, routingKey: "");
 
-            var consumer = new EventingBasicConsumer(this.channel);
+            // we create a consumer
+            var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (model, ea) =>
             {
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
                 var entity = JsonSerializer.Deserialize<Entity>(message);
                 Application.Current.Dispatcher.Invoke((Action)delegate {
-                    // Create a red Ellipse.
-                    Ellipse myEllipse = new Ellipse();
-
-                    // Create a SolidColorBrush with a red color to fill the
-                    // Ellipse with.
-                    SolidColorBrush mySolidColorBrush = new SolidColorBrush();
-
-                    // Describes the brush's color using RGB values.
-                    // Each value has a range of 0-255.
-                    mySolidColorBrush.Color = Color.FromRgb(255, 0, 0);
-                    myEllipse.Fill = mySolidColorBrush;
-                    myEllipse.StrokeThickness = 2;
-                    myEllipse.Stroke = Brushes.Black;
-                    // Set the width and height of the Ellipse.
-                    myEllipse.Width = 25;
-                    myEllipse.Height = 25;
-                    var text = new TextBlock();
-                    text.Text = entity.Name;
-                    Canvas.SetTop(myEllipse, entity.Y);
-                    Canvas.SetLeft(myEllipse, entity.X);
-                    Canvas.SetTop(text, entity.Y + 26);
-                    Canvas.SetLeft(text, entity.X);
-
-
-                    EntitiesMap.Children.Add(myEllipse);
-                    EntitiesMap.Children.Add(text);
+                    AddEventToEntitiesMap(entity);
                 });
             };
             
-            this.channel.BasicConsume(queue: result.QueueName,
+            channel.BasicConsume(queue: result.QueueName,
                      autoAck: true,
                      consumer: consumer);
-
 
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void AddEventToEntitiesMap(Entity? entity)
         {
+            if (entity == null)
+            {
+                return;
+            }
 
+            Ellipse el = new Ellipse();
+            SolidColorBrush brush = new SolidColorBrush();
+
+            brush.Color = Color.FromRgb(255, 0, 0);
+            el.Fill = brush;
+            el.StrokeThickness = 2;
+            el.Stroke = Brushes.Black;
+            el.Width = 25;
+            el.Height = 25;
+            var text = new TextBlock();
+            text.Text = entity.Name;
+            Canvas.SetTop(el, entity.Y);
+            Canvas.SetLeft(el, entity.X);
+            Canvas.SetTop(text, entity.Y + 26);
+            Canvas.SetLeft(text, entity.X);
+
+            EntitiesMap.Children.Add(el);
+            EntitiesMap.Children.Add(text);
         }
     }
 }
